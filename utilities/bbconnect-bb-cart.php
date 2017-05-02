b5b64341e61548e0bd559654218a3a780136e4b9
Mark Parnell <markparnell@brownbox.net.au>
2017-05-02 14:49:00 +1000

    Add note for any BB Cart transaction/checkout

--- utilities/bbconnect-bb-cart.php
+++ utilities/bbconnect-bb-cart.php
@@ -24,5 +24,17 @@ function bbconnect_bb_cart_post_purchase($cart_items, $entry, $form, $transactio
         update_user_meta($user->ID, 'bbconnect_kpi_transaction_count', $previous_count+1);
         update_user_meta($user->ID, 'bbconnect_kpi_last_transaction_date', bbconnect_get_current_datetime()->format('Y-m-d'));
         update_user_meta($user->ID, 'bbconnect_kpi_days_since_last_transaction', 0);
+
+        // Add note to user record
+        $post = array(
+                'post_title'    => 'Transaction - '.$form['title'],
+                'post_status'   => 'publish',
+                'post_type'     => 'bb_note',
+                'post_content'  => 'Transaction for $'.$total.' processed successfully. <a href="/wp-admin/admin.php?page=gf_entries&view=entry&id='.$entry['form_id'].'&lid='.$entry['id'].'">Entry details can be viewed here.</a>',
+                'post_author'   => $user->ID,
+        );
+
+        $post_id = wp_insert_post($post);
+        wp_set_object_terms($post_id, array('system', 'transaction'), 'bb_note_type');
     }
 }
