8932748671a6958fefd1c1ed2661c2cb6feb511e
Mark Parnell <markparnell@brownbox.net.au>
2017-05-03 13:45:09 +1000

    Add note for successful recurring Paydock transaction

--- utilities/bbconnect-bb-cart.php
+++ utilities/bbconnect-bb-cart.php
@@ -38,3 +38,28 @@ function bbconnect_bb_cart_post_purchase($cart_items, $entry, $form, $transactio
         wp_set_object_terms($post_id, array('system', 'transaction'), 'bb_note_type');
     }
 }
+
+add_action('bb_cart_webhook_paydock_recurring_success', 'bbconnect_bb_cart_webhook_paydock_recurring_success', 10, 2);
+function bbconnect_bb_cart_webhook_paydock_recurring_success($user, $amount) {
+    if ($user instanceof WP_User) {
+        $previous_total = (float)get_user_meta($user->ID, 'bbconnect_kpi_transaction_amount', true);
+        $previous_count = (int)get_user_meta($user->ID, 'bbconnect_kpi_transaction_count', true);
+
+        update_user_meta($user->ID, 'bbconnect_kpi_transaction_amount', $amount+$previous_total);
+        update_user_meta($user->ID, 'bbconnect_kpi_transaction_count', $previous_count+1);
+        update_user_meta($user->ID, 'bbconnect_kpi_last_transaction_date', bbconnect_get_current_datetime()->format('Y-m-d'));
+        update_user_meta($user->ID, 'bbconnect_kpi_days_since_last_transaction', 0);
+
+        // Add note to user record
+        $post = array(
+                'post_title'    => 'Automated Recurring Transaction Success',
+                'post_status'   => 'publish',
+                'post_type'     => 'bb_note',
+                'post_content'  => 'Transaction for $'.$amount.' processed successfully via PayDock.',
+                'post_author'   => $user->ID,
+        );
+
+        $post_id = wp_insert_post($post);
+        wp_set_object_terms($post_id, array('system', 'transaction'), 'bb_note_type');
+    }
+}
