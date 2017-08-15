c057ba257c39b31bba7c5de36ccc33e0b203c6ec
Mark Parnell <markparnell@brownbox.net.au>
2017-08-15 11:39:55 +1000

    Added pre-render of user details for Gravity Forms

--- utilities/bbconnect-forms.php
+++ utilities/bbconnect-forms.php
@@ -4,6 +4,82 @@
  */
 
 if (class_exists('GFAPI')) {
+    add_filter('gform_pre_render', 'bbconnect_populate_form_with_user_details');
+    /**
+     * Pre-render form with user data based on key
+     * @param array $form
+     * @return array $form
+     */
+    function bbconnect_populate_form_with_user_details($form) {
+        if (is_admin() && isset($_GET['user_id'])) { // If we're in WP Admin and have a user ID in querystring let's start there
+            $user = get_user_by('id', (int)$_GET['user_id']);
+        } else {
+            $user = wp_get_current_user(); // Otherwise if user is logged in we'll use them
+        }
+        $user = apply_filters('bbconnect_identify_user', $user); // Allow override by other plugins etc
+        if ($user instanceof WP_User) {
+            // Get user meta for later
+            $usermeta = get_user_meta($user->ID);
+
+            // Phone numbers are a special case
+            $phone_number = '';
+            $phone_data = maybe_unserialize($usermeta['telephone'][0]);
+            foreach ($phone_data as $existing_phone) {
+                if (!empty($existing_phone['value'])) {
+                    $phone_number = $existing_phone['value'];
+                    break;
+                }
+            }
+            foreach ($form['fields'] as &$field) {
+                // Standard fields
+                switch ($field->type) {
+                    case 'email':
+                        $field->defaultValue = $user->user_email;
+                        break;
+                    case 'name':
+                        foreach ($field->inputs as &$input) {
+                            if ($input['id'] == $field->id.'.3') {
+                                $input['defaultValue'] = $user->user_firstname;
+                            } elseif ($input['id'] == $field->id.'.6') {
+                                $input['defaultValue'] = $user->user_lastname;
+                            }
+                        }
+                        break;
+                }
+
+                // Fields mapped to user meta
+                if (!empty($field->inputs)) {
+                    foreach ($field->inputs as &$input) {
+                        if (!empty($input['usermeta_key'])) {
+                            switch ($input['usermeta_key']) {
+                                case 'telephone':
+                                    $input['defaultValue'] = $phone_number;
+                                    break;
+                                default:
+                                    if (isset($usermeta[$input['usermeta_key']])) {
+                                        $input['defaultValue'] = $usermeta[$input['usermeta_key']][0];
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+                } elseif (!empty($field->usermeta_key)) {
+                    switch ($field->usermeta_key) {
+                        case 'telephone':
+                            $field->defaultValue = $phone_number;
+                            break;
+                        default:
+                            if (isset($usermeta[$field->usermeta_key])) {
+                                $field->defaultValue = $usermeta[$field->usermeta_key][0];
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+        return $form;
+    }
+
     add_filter('gform_pre_render', 'bb_crm_pre_render_states');
     add_filter('gform_admin_pre_render', 'bb_crm_pre_render_states');
     add_filter('gform_pre_submission_filter', 'bb_crm_pre_render_states');
@@ -179,6 +255,25 @@ if (class_exists('GFAPI')) {
             $entry['created_by'] = $user_id;
             GFAPI::update_entry($entry, $entry['id']);
         }
+    }
+
+    // Output page for GF completion in admin
+    function bbconnect_submit_gravity_form() {
+        echo '<div class="wrap">'."\n";
+        $form_id = (int)$_GET['form_id'];
+        $user_id = (int)$_GET['user_id'];
+        if ($form_id && $user_id) {
+            $user = get_user_by('id', $user_id);
+            $form = GFAPI::get_form($form_id);
+            if ($user && $form) {
+                echo '<h1>'.$user->display_name.' ('.$user->user_email.')</h1>'."\n";
+                echo '<p>When you are done just close this tab to return to the user profile.</p>'."\n";
+                gravity_form($form_id);
+                return;
+            }
+        }
+        echo '<p>Invalid request. Please try again.</p>';
+        echo '</div>'."\n";
     }
 
     add_action('gform_field_advanced_settings', 'bb_crm_field_settings', 10, 2);
