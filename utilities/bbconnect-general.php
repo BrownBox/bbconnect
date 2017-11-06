00f34f462ba9c6857c5171b1200cecbc6ccb799a
Mark Parnell <markparnell@brownbox.net.au>
2017-11-07 10:30:42 +1100

    Added new helper function for inserting multiple rows into DB in single query

--- utilities/bbconnect-general.php
+++ utilities/bbconnect-general.php
@@ -409,3 +409,76 @@ function bbconnect_hello_dolly( $id, $args = null ) {
 	return $_hello_dolly;
 
 }
+
+/**
+ * A method for inserting multiple rows into the specified table
+ * Usage Example:
+ *   $insert_arrays = array();
+ *   foreach($assets as $asset) {
+ *       $insert_arrays[] = array(
+ *           'type' => "multiple_row_insert",
+ *           'status' => 1,
+ *           'name'=>$asset,
+ *           'added_date' => current_time('mysql'),
+ *           'last_update' => current_time('mysql'),
+ *       );
+ * }
+ * wp_insert_rows($insert_arrays);
+ * @param string $wp_table_name
+ * @param array $row_arrays
+ * @return boolean
+ *
+ * @author Ugur Mirza ZEYREK
+ * @source http://stackoverflow.com/a/12374838/1194797
+ */
+function bbconnect_insert_rows($wp_table_name, array $row_arrays) {
+    global $wpdb;
+    $wp_table_name = esc_sql($wp_table_name);
+    // Setup arrays for Actual Values, and Placeholders
+    $values = array();
+    $place_holders = array();
+    $query = "";
+    $query_columns = "";
+
+    $query .= "INSERT INTO {$wp_table_name} (";
+
+    foreach ($row_arrays as $count => $row_array) {
+        foreach ($row_array as $key => $value) {
+            if ($count == 0) {
+                if ($query_columns) {
+                    $query_columns .= "," . $key . "";
+                } else {
+                    $query_columns .= "" . $key . "";
+                }
+            }
+
+            $values[] = $value;
+
+            if (is_numeric($value)) {
+                if (isset($place_holders[$count])) {
+                    $place_holders[$count] .= ", '%d'";
+                } else {
+                    $place_holders[$count] .= "( '%d'";
+                }
+            } else {
+                if (isset($place_holders[$count])) {
+                    $place_holders[$count] .= ", '%s'";
+                } else {
+                    $place_holders[$count] .= "( '%s'";
+                }
+            }
+        }
+        // mind closing the GAP
+        $place_holders[$count] .= ")";
+    }
+
+    $query .= " $query_columns ) VALUES ";
+
+    $query .= implode(', ', $place_holders);
+
+    if ($wpdb->query($wpdb->prepare($query, $values))) {
+        return true;
+    } else {
+        return false;
+    }
+}
