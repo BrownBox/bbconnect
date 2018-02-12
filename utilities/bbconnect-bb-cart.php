de12551dcb44793f1d4ff06d5edb0f60e0a3d970
Mark Parnell <markparnell@brownbox.net.au>
2018-02-12 16:29:05 +1100

    Added support for new bb_cart_post_import hook

--- utilities/bbconnect-bb-cart.php
+++ utilities/bbconnect-bb-cart.php
@@ -47,3 +47,20 @@ function bbconnect_bb_cart_webhook_paydock_recurring_success($user, $amount, $tr
         bbconnect_insert_user_note($user->ID, 'Automated Recurring Transaction Success', $description, array('type' => 'system', 'subtype' => 'transaction'), $transaction_id);
     }
 }
+
+add_action('bb_cart_post_import', 'bbconnect_bb_cart_post_import', 10, 3);
+function bbconnect_bb_cart_post_import($user, $amount, $transaction_id = null) {
+    if ($user instanceof WP_User) {
+        $previous_total = (float)get_user_meta($user->ID, 'bbconnect_kpi_transaction_amount', true);
+        $previous_count = (int)get_user_meta($user->ID, 'bbconnect_kpi_transaction_count', true);
+
+        update_user_meta($user->ID, 'bbconnect_kpi_transaction_amount', $amount+$previous_total);
+        update_user_meta($user->ID, 'bbconnect_kpi_transaction_count', $previous_count+1);
+        update_user_meta($user->ID, 'bbconnect_kpi_last_transaction_date', bbconnect_get_current_datetime()->format('Y-m-d'));
+        update_user_meta($user->ID, 'bbconnect_kpi_days_since_last_transaction', 0);
+
+        // Add note to user record
+        $description = 'Transaction for $'.$amount.' imported successfully.';
+        bbconnect_insert_user_note($user->ID, 'Imported Transaction', $description, array('type' => 'system', 'subtype' => 'transaction'), $transaction_id);
+    }
+}
