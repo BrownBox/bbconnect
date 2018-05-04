8833dcac67122807d278f2fe28d64b7968100c7e
Mark Parnell <markparnell@brownbox.net.au>
2018-05-04 14:10:47 +1000

    Added quicklink to allow editing of user profile with default WP interface

---
+++ utilities/quicklinks/profile/1_edit_with_wordpress.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Edit With Wordpress quicklink
+ * @author markparnell
+ */
+class profile_1_edit_with_wordpress_quicklink extends bb_page_quicklink {
+    public function __construct() {
+        parent::__construct();
+        $user_id = $_GET['user_id'];
+        if (get_current_user_id() == $user_id) {
+            $edit_link = get_edit_profile_url($user_id);
+        } else {
+            $edit_link = add_query_arg('user_id', $user_id, self_admin_url('user-edit.php'));
+        }
+        $edit_link = add_query_arg('no_redirect', '1', $edit_link);
+        $this->url = $edit_link;
+        $this->title = 'Edit with Wordpress';
+    }
+}
